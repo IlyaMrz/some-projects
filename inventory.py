952f5976994fa48a7283f8a2ce9ecb4e59f8aefd
IlyaMrz <knpkaa@gmail.com>
2020-03-05 14:05:36 +0300

    function to add loot list to inventory

--- inventory.py
+++ inventory.py
@@ -1,13 +1,21 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
 
 def displayInventory(inventory):
-    print('Inventory:')
+    print('\nInventory:')
     item_total = 0
     for k, v in inventory.items():
         print(k, v)
         item_total += int(v)
-    print(f"Total number of items in inventory is: {item_total}")
+    print(f"\nTotal number of items in inventory is: {item_total}")
 
 
+def addToInventory(inventory, addItems):
+    for item in addItems:
+        inventory.setdefault(item, 0)
+        inventory[item] = inventory[item] + 1
+
+
+addToInventory(stuff, dragonLoot)
 displayInventory(stuff)
