04459ce301e4e9b8363db24cfdce4f3040274eae
IlyaMrz <knpkaa@gmail.com>
2020-10-16 20:38:14 +0300

    implemented login with cookies to bypass Udemy anti-Human detection

--- MultipleSitesUdemyScrape&Enroll.py
+++ MultipleSitesUdemyScrape&Enroll.py
@@ -27,6 +27,7 @@ from selenium.webdriver.chrome.options import Options
 
 # change path to your user chrome data (to use cookies)
 # Prabobly u need just change "pk111" to your PC user name
+# First time u need log in manually
 chrome_options = Options()
 chrome_options.add_argument(
     "user-data-dir=C:\\Users\\pk111\\AppData\\Local\\Google\\Chrome\\")
--- test2.py
+++
@@ -1,7 +0,0 @@
-a, b, sccss = 1, 2, 3
-print('==============================================================')
-print(
-    f'done! Scraped {a} links, and tried to enroll {b} unique links.')
-print(
-    f'____ Successfully enrolled {sccss} new courses! (or a few less) ____')
-print('==============================================================')
