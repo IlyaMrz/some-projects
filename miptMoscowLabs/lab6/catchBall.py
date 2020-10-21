2049a6b390fb0cc2ce5b0b4ffa77504932c8dca8
IlyaMrz <knpkaa@gmail.com>
2020-10-22 00:44:29 +0300

    commit

---
+++ miptMoscowLabs/lab6/catchBall.py
@@ -0,0 +1,73 @@
+import pygame
+from pygame.draw import *
+from random import randint
+import askname
+playerName = askname.main()
+# pygame.init()
+
+FPS = 2
+screen = pygame.display.set_mode((1200, 900))
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+BLACK = (0, 0, 0)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
+
+points = 0
+
+
+def new_ball():
+    global x, y, r
+    x = randint(100, 1050)
+    y = randint(100, 800)
+    r = randint(99, 130)
+    color = COLORS[randint(0, 5)]
+    circle(screen, color, (x, y), r)
+
+
+def new_rect():
+    global x, y, r
+    x = randint(50, 250)
+    y = randint(100, 500)
+    color = COLORS[randint(0, 5)]
+    rect(screen, color, (x, y, x, y))
+
+
+def click(event):
+    global points
+    ex, ey = event.pos
+    if (ey - y)**2 + (ex - x)**2 <= r**2:
+        print('YES!')
+        points += 1
+        print(points)
+    else:
+        print('Missed')
+
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            click(event)
+            print('Click!')
+    new_ball()
+    new_ball()
+    new_rect()
+    pygame.display.update()
+    screen.fill(BLACK)
+
+
+print(f'Player name: {playerName}. Score: {points}')
+pygame.quit()
