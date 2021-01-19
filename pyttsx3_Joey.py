2f31e58dc67c00bdd4cc9de07fe9beb4bbbefe82
morozov ilya <knpkaa@gmail.com>
2021-01-19 21:20:45 +0300

    speed as second sys.argv with default when no arg given

--- pyttsx3_Joey.py
+++ pyttsx3_Joey.py
@@ -1,9 +1,11 @@
 import sys
 import pyttsx3
 text = sys.argv[1]
+try: speed = int(sys.argv[2])
+except: speed = 245
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[3].id) # Joey IVONA 2 en
-engine.setProperty('rate', 245)
+engine.setProperty('rate', speed)
 engine.say(text)
 engine.runAndWait()
