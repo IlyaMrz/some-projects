0e4e3564c01bb2101352e0dd08ef8022f876fba6
morozov ilya <knpkaa@gmail.com>
2021-03-23 19:31:29 +0300

    some adj.

--- youtube_dl_mp3s/main.py
+++ youtube_dl_mp3s/main.py
@@ -2,10 +2,12 @@ import os
 print('Youtube Downloader'.center(40, '_'))
 URL = input('Enter youtube url :  ')
 id = URL.split('/watch?v=')[1]
-command = f'cmd /k youtube-dl -x --audio-format opus {id}'
-print(command)
+command = f'cmd /k youtube-dl -x -f bestaudio {URL}'
+# print(command)
 os.system(f'{command}')
 
+# https://wiki.archlinux.org/index.php/Youtube-dl  docs
+# https://github.com/ytdl-org/youtube-dl/blob/master/README.md#readme
 # youtube-dl -x --audio-format opus shXAjhcXP58
 # command for bat
 # python "C:\Games\VScodeProjects\some-projects\youtube_dl_mp3s\main.py"
