a6d5cf09112fb242114786d5d4907ba186da006c
morozov ilya <knpkaa@gmail.com>
2021-10-11 10:34:43 +0300

    from youtube_dl to yt-dlp

--- youtube_dl_mp3s/main.py
+++ youtube_dl_mp3s/main.py
@@ -2,7 +2,7 @@ import os
 print('Youtube Downloader'.center(40, '_'))
 URL = input('Enter youtube url :  ')
 id = URL.split('/watch?v=')[1]
-# command = f'cmd /k youtube-dl -x -f bestaudio {URL}' # default with youtube_ID a the end
+# command = f'cmd /k yt-dlp -x -f bestaudio {URL}' # default with youtube_ID a the end
 command = f'cmd /k youtube-dl -o "%(title)s.%(ext)s" -x -f bestaudio {URL}'
 # print(command)
 os.system(f'{command}')
