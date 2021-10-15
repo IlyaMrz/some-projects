19289b771e4ab111234041aec7457a32bb790154
morozov ilya <knpkaa@gmail.com>
2021-10-15 08:52:54 +0300

    auto clipboard getter instead of user input

--- yt_dlp_to_vlc/720p.py
+++ yt_dlp_to_vlc/720p.py
@@ -1,7 +1,7 @@
 # from __future__ import unicode_literals
 import subprocess
 from yt_dlp import YoutubeDL
-
+import win32clipboard
 
 VLC_PATH="C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"
 
@@ -9,8 +9,11 @@ ydl = YoutubeDL({
     'format': '22',
 })
 
-print('Youtube_to_VLC'.center(70, '_'))
-URL = input('Enter youtube url :  ')
+# print('Youtube_to_VLC'.center(70, '_'))
+# URL = input('Enter youtube url :  ')
+win32clipboard.OpenClipboard()
+URL = win32clipboard.GetClipboardData()
+win32clipboard.CloseClipboard()
 
 r = ydl.extract_info(URL, download=False)
 
