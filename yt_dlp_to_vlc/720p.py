b334048f1d6ba66f08ac4634c0cf3c11ad45aa34
morozov ilya <knpkaa@gmail.com>
2021-10-15 05:36:04 +0300

    yt to vlc

--- yt_dlp_to_vlc/720p.py
+++ yt_dlp_to_vlc/720p.py
@@ -1,5 +1,4 @@
 from __future__ import unicode_literals
-# from pprint import pprint
 import subprocess
 from yt_dlp import YoutubeDL
 ydl = YoutubeDL({
@@ -10,7 +9,5 @@ URL = input('Enter youtube url :  ')
 r = ydl.extract_info(URL, download=False)
 
 link = (r['url'])
-# pprint(link)
 command=f'"C:\\Program Files\\VideoLAN\\VLC\\vlc.exe" "{link}"'
-# print(command)
 subprocess.call(command)
