1de187942c297411a22b277fff124292412d57b5
morozov ilya <knpkaa@gmail.com>
2021-10-15 11:19:03 +0300

    arg id format from clipboard and default auto video format iteration

--- yt_dlp_to_vlc/1080p.py
+++ yt_dlp_to_vlc/1080p.py
@@ -2,38 +2,52 @@
 import subprocess
 from yt_dlp import YoutubeDL
 import win32clipboard
+from pprint import pprint
 
 # youtub's format_ID:
 # https://gist.github.com/AgentOak/34d47c65b1d28829bb17c24c04a0096f#dash-video
 # or check formats with command:
 # yt-dlp -F YT_URL
 # 299 1080 - 60fps
-# 136 1080 - 30fps
+# 137 1080 - 30fps
+# if u wanna use certain format use clipboard => URL,ID
 
-VLC_PATH="C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"
+VLC_PATH = "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"
+
 
 def get_Data_with_format(format_ID):
     return YoutubeDL({
         'format': format_ID,
-        })
+        "player_client": "android",
+    })
+
 
 ydl_a = get_Data_with_format('251')
 
+video_formats = ['137', '248', '299', '303', '335', '399', '699']  # 1080
 
-try:
-    ydl_v = get_Data_with_format('136')
-    # print('Youtube_to_VLC'.center(70, '_'))
-    # URL = input('Enter youtube url :  ')
-    win32clipboard.OpenClipboard()
-    URL = win32clipboard.GetClipboardData()
-    win32clipboard.CloseClipboard()
+win32clipboard.OpenClipboard()
+URL = win32clipboard.GetClipboardData()
+win32clipboard.CloseClipboard()
 
-    audio_url = ydl_a.extract_info(URL, download=False)['url']
-    video_url = ydl_v.extract_info(URL, download=False)['url']
+idx = 0
+if len(URL.split(',')) == 2:
+    URL, idxx = URL.split(',')
+    idx = video_formats.index(str(idxx))
 
-except:
-    ydl_v = get_Data_with_format('299')
-    video_url = ydl_v.extract_info(URL, download=False)['url']
-finally:
-    command=f'{VLC_PATH} {video_url} --input-slave={audio_url}'
-    subprocess.call(command)
+audio_url = ydl_a.extract_info(URL, download=False)['url']
+
+
+def go(idx):
+    try:
+        ydl_v = get_Data_with_format(video_formats[idx])
+        video_url = ydl_v.extract_info(URL, download=False)['url']
+        return video_url
+    except:
+        idx = idx+1
+        return go(idx)
+
+
+video_url = go(idx)
+command = f'{VLC_PATH} {video_url} --input-slave={audio_url}'
+subprocess.call(command)
