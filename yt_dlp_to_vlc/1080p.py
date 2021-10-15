899578c5e743117cbb3f12ada1c61b6429cbb913
morozov ilya <knpkaa@gmail.com>
2021-10-15 06:46:33 +0300

    YT to VLC

--- yt_dlp_to_vlc/1080p.py
+++ yt_dlp_to_vlc/1080p.py
@@ -2,6 +2,8 @@
 import subprocess
 from yt_dlp import YoutubeDL
 
+VLC_PATH="C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"
+
 ydl_a = YoutubeDL({
     'format': '251',
 })
@@ -13,7 +15,5 @@ URL = input('Enter youtube url :  ')
 audio = ydl_a.extract_info(URL, download=False)['url']
 video = ydl_v.extract_info(URL, download=False)['url']
 
-command=f'"C:\\Program Files\\VideoLAN\\VLC\\vlc.exe" {video} --input-slave={audio}'
+command=f'{VLC_PATH} {video} --input-slave={audio}'
 subprocess.call(command)
-
-
--- yt_dlp_to_vlc/720p.py
+++ yt_dlp_to_vlc/720p.py
@@ -1,13 +1,19 @@
 # from __future__ import unicode_literals
 import subprocess
 from yt_dlp import YoutubeDL
+
+
+VLC_PATH="C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"
+
 ydl = YoutubeDL({
     'format': '22',
 })
+
 print('Youtube_to_VLC'.center(70, '_'))
 URL = input('Enter youtube url :  ')
+
 r = ydl.extract_info(URL, download=False)
 
 link = (r['url'])
-command=f'"C:\\Program Files\\VideoLAN\\VLC\\vlc.exe" "{link}"'
+command=f'{VLC_PATH} "{link}"'
 subprocess.call(command)
