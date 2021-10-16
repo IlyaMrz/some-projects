ea6672e93b5ef4eb1794b620b007a7425b3c582f
morozov ilya <knpkaa@gmail.com>
2021-10-16 19:24:51 +0300

    clr

--- youtube_dl_mp3s/main.py
+++ youtube_dl_mp3s/main.py
@@ -8,7 +8,7 @@ win32clipboard.CloseClipboard()
 if not "youtube.com/watch" in URL:
     print('Youtube Downloader'.center(40, '_'))
     URL = input('Enter youtube url :  ')
-id = URL.split('/watch?v=')[1]
+
 # command = f'cmd /k yt-dlp -x -f bestaudio {URL}' # default with youtube_ID a the end
 command = f'cmd /k yt-dlp -o "%(title)s.%(ext)s" -x --audio-format best --embed-thumbnail --add-metadata {URL}'
 # print(command)
