d2ab86d2ae2f5b1b8455a3b8febe849c261d373a
morozov ilya <knpkaa@gmail.com>
2021-10-21 09:37:48 +0300

    adds count for a collective money

--- flip_game.py
+++ flip_game.py
@@ -1,13 +1,16 @@
 import random
 import sys
 from pprint import pprint
+import functools
 sys.setrecursionlimit(10**6)
 
 # https://www.youtube.com/watch?v=f1vXAHGIpfc
 
 money = 100
 players_count = 1000
-flips_per_player = 700
+flips_per_player = 500
+
+collective_money_onStart = money*players_count
 
 
 def flip(m, round):
@@ -36,4 +39,8 @@ for k in array:
     if k > money:
         positive_count = positive_count+1
 
+collective_money_onEnd = functools.reduce(lambda a, b: a+b, array)
+
 print(positive_count, ' players with positive cashflow')
+print('collective money on Start: ', collective_money_onStart)
+print('collective money in z End: ', collective_money_onEnd)
