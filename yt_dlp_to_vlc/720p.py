5bb0571b0d390721a98fcfb8f66b7eda82364c44
morozov ilya <knpkaa@gmail.com>
2021-10-22 09:54:45 +0300

    format <=720 incase -f 22 not available

--- yt_dlp_to_vlc/720p.py
+++ yt_dlp_to_vlc/720p.py
@@ -4,11 +4,7 @@ from yt_dlp import YoutubeDL
 import win32clipboard
 
 VLC_PATH = "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"
-COOKIE_PATH = '' #txt
-
-ydl = YoutubeDL({
-    'format': '22',
-})
+COOKIE_PATH = ''  # txt
 
 win32clipboard.OpenClipboard()
 URL = win32clipboard.GetClipboardData()
@@ -18,11 +14,22 @@ if not "youtube.com/watch" in URL:
     print('Youtube_to_VLC'.center(70, '_'))
     URL = input('Enter youtube url :  ')
 
-link = ydl.extract_info(URL, download=False)['url']
+
+def direct_link(format_ID):
+    yt_data = YoutubeDL({'format': format_ID})
+    link = yt_data.extract_info(URL, download=False)['url']
+    return link
+
+
+try:
+    link = direct_link('22')
+except:
+    link = direct_link('best[height<=720]')
+
 
 # mark as watched
 #command_mark_watched = f'yt-dlp --skip-download --cookies {COOKIE_PATH} --mark-watched {URL}'
 # subprocess.call(command_mark_watched)
 
-command = f'{VLC_PATH} "{link}"'
+command = f'{VLC_PATH} {link}'
 subprocess.call(command)
