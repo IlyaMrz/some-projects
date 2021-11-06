27f0e5d9cfd20e2d6acd90763985e20f31a23a96
ilyamrz <knpkaa@gmail.com>
2021-11-07 00:27:25 +0300

    ability to open requested link right in VLC

--- yt_dlp_to_vlc/direct_link.py
+++ yt_dlp_to_vlc/direct_link.py
@@ -3,11 +3,15 @@ from yt_dlp import YoutubeDL
 import win32clipboard
 
 
+VLC_PATH = "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe"
+
+
 def direct_link(format_ID):
     yt_data = YoutubeDL({'format': format_ID})
     link = yt_data.extract_info(URL, download=False)['url']
     print(link)
-    #return link
+    return link
+
 
 win32clipboard.OpenClipboard()
 URL = win32clipboard.GetClipboardData()
@@ -19,7 +23,18 @@ subprocess.call(command_get_formats)
 
 print('Choose a format'.center(70, '_'))
 ur_format_ID = input('Enter your format :  ')
+user_input = 'input'
 
-while ur_format_ID:
-    direct_link(ur_format_ID)
-    ur_format_ID = input('Press enter to exit... or format ID to run again:  ')
+while user_input:
+    link = direct_link(ur_format_ID)
+    user_input = input("""
+    Press enter to exit... 
+    or format ID to run again:  
+    or g to run link above in VLC:
+    
+    """)
+    if user_input == 'g':
+        command = f'{VLC_PATH} {link}'
+        subprocess.call(command)
+    elif user_input:
+        ur_format_ID = user_input
