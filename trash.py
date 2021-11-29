15a4950684bc9e4e8be6965f419652d209e7503c
ilyamrz <knpkaa@gmail.com>
2021-11-29 23:25:59 +0300

    trash

---
+++ trash.py
@@ -0,0 +1,41 @@
+# some stuff testing wtch зай
+firstM_in_P = ["Zay1ianN8hA6VVncaafKBxipvT", ]
+print("""
+ca9aba84
+6ac9681a
+aa66d380
+11e91334
+97ea18ef
+98e5ac02
+2d01c775
+0955e188
+09b78337
+d15fe8ad
+a0eb5ab7
+1585caa4 -  u
+4723781b
+83dc9c12 - u
+4123b9f2 -  u
+2685f9af -  u
+
+nEw 27-11-2021:
+9daec0e3
+ae182554
+7715dbd9
+4a2bcb2f
+213bff94
+ea80c4b1
+b2e6ffd7
+0eacbe0e
+81612691
+0e18393a
+e057a14c
+90dc6cc3
+07428c40
+cf0126ca
+0ac81faa
+2d6229ac""")
+secondCl = ["Zay1!gwHh:S6KArF4Mb"]
+print("HEROSDpV39RC_J9JeBYn8A")
+
+print("obfusc")
